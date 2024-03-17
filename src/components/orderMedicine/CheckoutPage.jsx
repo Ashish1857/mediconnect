d8cd1a5f773d92027014b6caef5df32774d38f87
XavierDai <113158408+XavierDai@users.noreply.github.com>
2024-03-17 18:00:58 -0400

    Xavier dai medicine order layout and merge back-end stuff (#23)
    * fix
    
    * order medicine page all func
    
    * order medicine page all fuc
    
    * clear warning
    
    * fix 3/7 problems
    
    * fix number
    
    * 1
    
    * fix UI problems

--- src/components/orderMedicine/CheckoutPage.jsx
+++ src/components/orderMedicine/CheckoutPage.jsx
@@ -1,110 +1,69 @@
-import React, { useContext } from "react";
-import {
-  Grid,
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-  Button,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import { useCart } from "./CartContext";
-import BackButton from "./BackButton";
+import React, { useContext } from 'react';
+import { Grid, Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useCart } from './CartContext';
+import BackButton from './BackButton';
 
 const CheckoutPage = () => {
   const { cartItems, updateQuantity, removeItem } = useCart();
-  const totalPrice = cartItems.reduce(
-    (acc, item) => acc + item.price * item.quantity,
-    0
-  );
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
   return (
-    <div style={{ width: "80%", margin: "auto", marginTop: "5%" }}>
+    <div style={{ width: '80%', margin: 'auto', marginTop: '5%' }}>
+      <Typography variant="h6" sx={{ fontWeight: 'bold', color: 'rgb(23, 55, 84)', textAlign: 'left', marginLeft: '0px',marginBottom:'2vh' }}>
+        ITEM(S) IN YOUR CART:
+      </Typography>
+      <div style={{ borderTop: '1px solid #e0e0e0'}}>
       {cartItems.map((item) => (
-        <Card
-          key={item.id}
-          sx={{
-            display: "flex",
-            marginBottom: 2,
-            marginTop: 2,
-            padding: "0.5rem",
-          }}
-        >
-          <Link to={`/product?drug=${item.id}`}>
-            <CardMedia
-              component="img"
-              sx={{ width: 150, height: 200, objectFit: "cover" }}
-              image={item.image_url}
-              alt={item.name}
-            />
-          </Link>
-          <CardContent
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              width: "65%",
-              alignItems: "flex-start",
-            }}
-          >
-            <Typography gutterBottom variant="h5" component="div">
+        <Card key={item.id} sx={{
+          display: 'flex',
+          marginBottom: '0px',
+          marginTop: '0px',
+          boxShadow: 'none', // 去除阴影
+          borderLeft: 'none', // 去除左边框
+          borderRight: 'none', // 去除右边框
+          borderBottom: '1px solid #e0e0e0', // 设置下边框为淡灰色
+          padding: '16px', // 增加上下左向内的间距
+        }}>
+          <div style={{ width: 151, height: 151, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Link to={`/product?drug=${item.id}`}>
+              <CardMedia
+                component="img"
+                sx={{ height: 151, width: 'auto', maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }}
+                image={item.image_url}
+                alt={item.name}
+              />
+            </Link>
+          </div>
+          <CardContent sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, alignItems: 'flex-start' }}>
+            <Typography gutterBottom sx={{ fontWeight: 'bold' }} component="div">
               {item.name}
             </Typography>
-            <Typography
-              variant="body2"
-              style={{ color: item.stock === "in_stock" ? "green" : "red" }}
-            >
+            <Typography variant="body2" style={{ color: item.stock === 'in_stock' ? 'green' : 'red' }}>
               {item.stock}
             </Typography>
-            <Typography variant="body1">{item.description}</Typography>
-            <Grid
-              container
-              spacing={2}
-              style={{ margin: "1rem 0" }}
-              alignItems="baseline"
-            >
-              <Grid
-                item
-                style={{
-                  padding: "0.25rem",
-                  border: "1px solid lightblue",
-                  borderRadius: "5px",
-                  margin: "0.25rem",
-                }}
-              >
-                <Button
-                  variant="outlined"
-                  onClick={() => updateQuantity(item.id, "decrease")}
-                >
-                  -
-                </Button>
-                <Typography display="inline">{` ${item.quantity} `}</Typography>
-                <Button
-                  variant="outlined"
-                  onClick={() => updateQuantity(item.id, "increase")}
-                >
-                  +
-                </Button>
-              </Grid>
+            <Typography variant="body1" sx={{ textAlign: 'left' }}>
+              {item.description}
+            </Typography>
+            <Grid container spacing={2}>
               <Grid item>
-                <Button variant="outlined" onClick={() => removeItem(item.id)}>
-                  Delete
-                </Button>
+                <Button onClick={() => updateQuantity(item.id, 'decrease')}>-</Button>
+                <Typography display="inline">{` ${item.quantity} `}</Typography>
+                <Button onClick={() => updateQuantity(item.id, 'increase')}>+</Button>
+              </Grid>
+
+              <Grid item>
+                <Button onClick={() => removeItem(item.id)}>Delete</Button>
               </Grid>
             </Grid>
           </CardContent>
-          <Typography
-            sx={{ width: "5%", textAlign: "right", fontWeight: "bold" }}
-          >
+          <Typography sx={{ alignSelf: 'center', marginRight: 2, fontWeight: 'bold' }}>
             ${item.price}
           </Typography>
         </Card>
       ))}
-      <Grid
-        container
-        justifyContent="flex-end"
-        spacing={2}
-        style={{ marginTop: "20px" }}
-      >
+    </div>
+      <Grid container justifyContent="flex-end" spacing={2} style={{ marginTop: "20px" }}>
         <Grid item xs={12} style={{ textAlign: "right" }}>
           <Typography variant="h6">Total: ${totalPrice.toFixed(2)}</Typography>
         </Grid>
@@ -112,17 +71,12 @@ const CheckoutPage = () => {
           <BackButton />
         </Grid>
         <Grid item>
-          <Button component={Link} to="/medicine" variant="outlined">
+          <Button component={Link} to="/orderWithoutPres" variant="contained" sx={{ bgcolor: 'lightblue', '&:hover': { bgcolor: '#add8e6' } }}>
             Add more
           </Button>
         </Grid>
         <Grid item>
-          <Button
-            component={Link}
-            to="/payment"
-            variant="contained"
-            color="primary"
-          >
+          <Button component={Link} to="/payment" variant="contained" color="primary">
             Checkout
           </Button>
         </Grid>
--- src/components/orderMedicine/OrderWithoutPres.jsx
+++ src/components/orderMedicine/OrderWithoutPres.jsx
@@ -1,181 +1,124 @@
-import React, { useState, useEffect } from "react";
-import {
-  Grid,
-  Card,
-  CardMedia,
-  CardContent,
-  Button,
-  Typography,
-  Autocomplete,
-  TextField,
-} from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import { Link } from "react-router-dom";
-import { useCart } from "./CartContext";
-import BackButton from "./BackButton";
+import React, { useState, useEffect } from 'react';
+import { Grid, Card, CardMedia, CardContent, Button, Typography, Autocomplete, TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useCart } from './CartContext';
+import BackButton from './BackButton';
 
 export const OrderWithoutPres = () => {
-  const [drugs, setDrugs] = useState([]);
-  const [originalDrugs, setOriginalDrugs] = useState([]); // Store the original drugs list
+    const [drugs, setDrugs] = useState([]);
+    const [originalDrugs, setOriginalDrugs] = useState([]);
+    const { cartItems, addToCart, removeItem, updateQuantity } = useCart();
+    const [inputValue, setInputValue] = useState("");
 
-  const { cartItems, addToCart, removeItem } = useCart();
+    useEffect(() => {
+        fetch('https://XavierDai.github.io/medicine.json')
+            .then(response => response.json())
+            .then(data => {
+                setDrugs(data);
+                setOriginalDrugs(data);
+            })
+            .catch(error => console.error('Error fetching drugs:', error));
+    }, []);
 
-  const [inputValue, setInputValue] = useState("");
+    const clearSearch = () => {
+        setInputValue('');
+        setDrugs(originalDrugs);
+    };
 
-  useEffect(() => {
-    fetch("https://XavierDai.github.io/medicine.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setDrugs(data);
-        setOriginalDrugs(data); // Set original drugs list
-      })
-      .catch((error) => console.error("Error fetching drugs:", error));
-  }, []);
+    const performSearch = (value) => {
+        if (value) {
+            const filteredDrugs = originalDrugs.filter(drug => drug.name.toLowerCase().includes(value.toLowerCase()));
+            setDrugs(filteredDrugs);
+        } else {
+            setDrugs(originalDrugs);
+        }
+    };
 
-  const clearSearch = () => {
-    setInputValue("");
-    setDrugs(originalDrugs); // Reset to original drugs list
-  };
-  const performSearch = (value) => {
-    // Filter or fetch data based on the search term
-    if (value) {
-      const filteredDrugs = originalDrugs.filter((drug) =>
-        drug.name.toLowerCase().includes(value.toLowerCase())
-      );
-      setDrugs(filteredDrugs);
-    } else {
-      setDrugs(originalDrugs);
-    }
-  };
-  return (
-    <div style={{ width: "80%", margin: "auto" }}>
-      <Grid
-        container
-        justifyContent="center"
-        alignItems="center"
-        style={{ marginBottom: "20px" }}
-      >
-        <Grid item xs={12} sm={6}>
-          <Autocomplete
-            freeSolo
-            inputValue={inputValue}
-            options={drugs.map((option) => option.name)}
-            onInputChange={(event, newInputValue) => {
-              // Perform search on input change
-              setInputValue(newInputValue);
-              performSearch(newInputValue);
-            }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Search medicine"
-                variant="outlined"
-                fullWidth
-              />
-            )}
-          />
-        </Grid>
-        <Button onClick={clearSearch} style={{ marginLeft: "10px" }}>
-          Clear search
-        </Button>
-      </Grid>
-      <Grid container spacing={3}>
-        {drugs.map((drug) => (
-          <Grid item xs={12} sm={6} md={6} key={drug.id}>
-            <Card
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                overflow: "hidden",
-                minHeight: "250px",
-                padding: "0.5rem",
-              }}
-            >
-              <Link to={`/product?drug=${drug.id}`}>
-                <CardMedia
-                  component="img"
-                  sx={{
-                    width: "150px",
-                    flexShrink: 0,
-                    height: "200px",
-                    objectFit: "cover",
-                  }} // 限制图片宽度，防止其撑大卡片
-                  image={drug.image_url}
-                  alt={drug.name}
-                />
-              </Link>
-              <CardContent
-                sx={{
-                  flex: "1",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "flex-start", // 确保内容靠左
-                  overflow: "hidden", // 防止内容溢出
-                  "& > *": {
-                    wordBreak: "break-word",
-                    overflowWrap: "break-word", // 确保文本在需要时可以换行
-                  },
-                }}
-              >
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                  sx={{ textAlign: "left", width: "100%" }}
-                >
-                  {drug.name}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                  sx={{ textAlign: "left", width: "100%" }}
-                >
-                  {drug.description}
-                </Typography>
-                <Typography
-                  variant="body1"
-                  color="text.primary"
-                  sx={{ textAlign: "left", width: "100%" }}
-                >
-                  {drug.price}
-                </Typography>
-                <br />
-                {!cartItems.find((item) => item.id === drug.id) ? (
-                  <Button
-                    size="small"
-                    color="primary"
-                    variant="outlined"
-                    onClick={() => addToCart(drug.id)}
-                  >
-                    Add to cart
-                  </Button>
-                ) : (
-                  <Button
-                    size="small"
-                    color="secondary"
-                    variant="outlined"
-                    onClick={() => removeItem(drug.id)}
-                  >
-                    Remove from cart
-                  </Button>
-                )}
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-      <Grid container justifyContent="flex-end" style={{ marginTop: "20px" }}>
-        <BackButton />
-        <Button
-          component={Link}
-          to="/checkout"
-          variant="contained"
-          color="primary"
-        >
-          Check Out
-        </Button>
-      </Grid>
-    </div>
-  );
+    return (
+        <div style={{ width: '80%', margin: 'auto' }}>
+            <Grid container justifyContent="center" alignItems="center" style={{ marginBottom: '20px' }}>
+                <Grid item xs={12} style={{ display: 'flex', alignItems: 'center' }}>
+                    <Autocomplete
+                        freeSolo
+                        inputValue={inputValue}
+                        onInputChange={(event, newInputValue) => {
+                            setInputValue(newInputValue);
+                            performSearch(newInputValue);
+                        }}
+                        options={drugs.map((option) => option.name)}
+                        renderInput={(params) => (
+                            <TextField {...params} label="Search medicine" variant="outlined" fullWidth />
+                        )}
+                        style={{ flexGrow: 1 }}
+                    />
+                    <Button onClick={clearSearch} style={{ marginLeft: '10px' }}>Clear search</Button>
+                </Grid>
+            </Grid>
+
+            <Grid container spacing={3}>
+                {drugs.map((drug) => {
+                    const inCart = cartItems.find(item => item.id === drug.id);
+                    return (
+                        <Grid item xs={6} sm={3} md={3} key={drug.id}>
+                            <Card sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                overflow: 'hidden',
+                                height: '100%',
+                                padding: '5px',
+                                border: 'none',
+                                width: 1,
+               
+                            }}>
+                                <Link to={`/product?drug=${drug.id}`}>
+                                    <CardMedia
+                                        component="img"
+                                        sx={{ width: '100%', height: 140 }}
+                                        image={drug.image_url}
+                                        alt={drug.name}
+                                    />
+                                </Link>
+                                <CardContent sx={{
+                                    flex: '1',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'flex-start',
+                                    justifyContent: 'space-between',
+                                    width: '100%',
+                                    padding: '16px',
+                                }}>
+                                    <Typography gutterBottom variant="h5" sx={{ width: '100%', textAlign: 'left', fontWeight: 'normal' }}>{drug.name}</Typography>
+                                    <Typography variant="body1" sx={{ width: '100%', textAlign: 'left', fontWeight: 'bold' }}>${drug.price}</Typography>
+                                    <div style={{ width: '100%', marginTop: '10px' }}>
+                                        {inCart ? (
+                                            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+                                                {/* 包裹加减按钮和数量显示在一个带边框的容器中 */}
+                                                <div style={{ display: 'flex', border: '1px solid #ccc', borderRadius: '4px', alignItems: 'center' }}>
+                                                    <Button variant="outlined" size="small" onClick={() => updateQuantity(drug.id, 'decrease')} sx={{ minWidth: '30px', border: '0px' }}>-</Button>
+                                                    <Typography display="inline" sx={{ margin: '0 1vw' }}>{inCart.quantity}</Typography>
+                                                    <Button variant="outlined" size="small" onClick={() => updateQuantity(drug.id, 'increase')} sx={{ minWidth: '30px', border: '0px' }}>+</Button>
+                                                </div>
+                                                <Button size="small" color="secondary" onClick={() => removeItem(drug.id)}>Remove</Button>
+                                            </div>
+                                        ) : (
+                                            <Button size="small" color="primary" onClick={() => addToCart(drug.id, 1)} sx={{ width: '100%' }}>Add to Cart</Button>
+                                        )}
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    );
+                })}
+            </Grid>
+
+
+
+
+            <Grid container justifyContent="flex-end" style={{ marginTop: '20px' }}>
+                <BackButton />
+                <Button component={Link} to="/checkout" variant="contained" color="primary">Check Out</Button>
+            </Grid>
+        </div>
+    );
 };
